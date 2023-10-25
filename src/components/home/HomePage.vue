bb4c90c9c9a49beee6525a0f89d4ee89c6a7ea1d
YanMiranda <felipeyaann@gmail.com>
2023-10-25 02:24:50 -0300

    carrousel + ajusting navbar

--- src/components/home/HomePage.vue
+++ src/components/home/HomePage.vue
@@ -1,15 +1,26 @@
 <template>
-  <el-carousel :interval="4000" type="card" height="400px">
-    <el-carousel-item>
-      <carouselCard1/>
-    </el-carousel-item>
-  </el-carousel>
+    <el-carousel class="custom-carousel" :interval="10000" type="card" height="580px">
+      <el-carousel-item>
+        <FitCard/>
+      </el-carousel-item>
+      <el-carousel-item>
+        <AdvancedCard/>
+      </el-carousel-item>
+      <el-carousel-item>
+        <PremiumCard/>
+      </el-carousel-item>
+    </el-carousel>
 </template>
 
 <script setup>
-import carouselCard1 from './carouselCards/carouselCard1.vue'
+import FitCard from './carouselCards/FitCard.vue'
+import AdvancedCard from './carouselCards/AdvancedCard.vue'
+import PremiumCard from './carouselCards/PremiumCard.vue'
 </script>
 
 <style scoped>
-
+.custom-carousel {
+  margin-top: 100px;
+  cursor: auto;
+}
 </style>
